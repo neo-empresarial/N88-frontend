615a8a6caefe19a8f99cd9bbbd765fde67d398d8
gustavobftorres <gbr@DESKTOP-7A7P33M>
2025-02-28 10:17:57 -0300

    add first idea of home page, plus basic motion logic

--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,30 @@ import { Badge } from "@/components/ui/badge";
 import Image from "next/image";
 import grade from "./assets/grade.png";
 import materias from "./assets/materias.png";
-import { useSpring, animated } from "@react-spring/web";
-import { useState } from "react";
-import { motion } from "motion/react";
+import { useEffect, useState } from "react";
+import { motion, useScroll } from "motion/react";
 
 export default function Home() {
   const [isMounted, setIsMounted] = useState(false);
+  const { scrollYProgress } = useScroll();
+  const [fade, setFade] = useState(0);
+
+  useEffect(() => {
+    // Subscribe to scroll progress
+    scrollYProgress.onChange((value) => {
+      // Map the scroll value to a fade effect
+      setFade(value);
+    });
+  }, [scrollYProgress]);
 
   return (
-    <div className="flex justify-center items-center min-h-screen ">
-      <div className="flex flex-col items-center justify-center w-full ">
+    <div className="flex justify-center items-center min-h-screen flex-col">
+      <div className="flex flex-col items-center justify-center w-full mt-[10%]">
         <motion.div
-          className="w-full h-100 flex items-center justify-center flex-col"
-          initial={{ y: 10, opacity: 0 }}
-          animate={{ y: -30, opacity: 1 }}
-          transition={{ duration: 1 }}
+          className="w-full h-50 flex items-center justify-center flex-col"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
         >
           <Badge variant="outline">Development by NEO Empresarial</Badge>
           <h1 className="text-6xl font-sans font-medium">Uma nova forma </h1>
@@ -37,21 +46,42 @@ export default function Home() {
           </p>
           <p>tudo em um mesmo lugar.</p>
         </motion.div>
-          <motion.div
-            className="flex flex-col items-center justify-center w-full mb-10"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 1 }}
-          >
-            <Image src={grade} alt="grade" width={500} height={500} />
-          </motion.div>
-          {/* <Image
-            className="absolute bottom-20 left-30"
-            src={materias}
-            alt="grade"
-            width={300}
-            height={300}
-          /> */}
+        <motion.div
+          className="flex flex-col items-center justify-center w-full mb-10"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+        >
+          <Image src={grade} alt="grade" width={500} height={500} />
+        </motion.div>
+      </div>
+
+      <div
+        className="grid grid-cols-2 items-center space-x-12 justify-center w-full mb-10 overflow-y-auto p-10"
+        style={{
+          background: `linear-gradient(to bottom, rgba(179, 179, 204, 0) 0%, rgba(179, 179, 204, ${fade}) 100%)`,
+        }}
+      >
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, type: "spring", bounce: 0.4 }}
+          className="border border-grey-200 p-10 rounded-xl bg-[#020817] flex flex-col items-center justify-center gap-4"
+        >
+          <h1 className="text-6xl font-sans font-medium">Avalie as matérias</h1>
+          <Image src={materias} alt="materias" width={500} height={500} />
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, type: "spring", bounce: 0.4 }}
+          className="border border-grey-200 p-10 rounded-xl bg-[#020817] flex flex-col items-center justify-center gap-4"
+        >
+          <h1 className="text-6xl font-sans font-medium w-full align-middle text-center">
+            Avalie os professores
+          </h1>
+          <Image src={materias} alt="materias" width={500} height={500} />
+        </motion.div>
       </div>
     </div>
   );
