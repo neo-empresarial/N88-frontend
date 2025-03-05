6527ebfa032732acdd3156b4c96eda14e6f5c592
kaiquevalentim <kaique.valentim@unesp.br>
2025-03-05 16:36:37 -0300

    Update: allow user without session to send a feedback, clean input field after feedback be send

--- app/feedback/FeedbackButton.tsx
+++ app/feedback/FeedbackButton.tsx
@@ -4,14 +4,11 @@ import { MessageSquare } from "lucide-react";
 import FeedbackForm from "./FeedbackForm";
 
 async function FeedbackButton() {
-  const session = await getSession();
   return (
-    session && (
-      <FloattingButton
-        icon={<MessageSquare size={25} />}
-        content={<FeedbackForm />}
-      />
-    )
+    <FloattingButton
+      icon={<MessageSquare size={25} />}
+      content={<FeedbackForm />}
+    />
   );
 }
 
--- app/feedback/FeedbackForm.tsx
+++ app/feedback/FeedbackForm.tsx
@@ -5,10 +5,17 @@ import { Textarea } from "@/components/ui/textarea";
 import { useFormState } from "react-dom";
 import { sendFeedback } from "./useFeedbackForm";
 import SubmitButton from "@/components/submit-button";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 const FeedbackForm = () => {
+  const inputRef = useRef<HTMLTextAreaElement>(null);
   const [state, action] = useFormState(sendFeedback, undefined);
+
+  useEffect(() => {
+    if (state?.success && inputRef.current) {
+      inputRef.current.value = "";
+    }
+  }, [state]);
 
   return (
     <form className="grid gap-2" action={action}>
@@ -20,13 +27,16 @@ const FeedbackForm = () => {
         Seu feedback é muito importante!
       </Label>
 
-      {state?.success && <p className="text-sm text-green-600">Muito obrigado!</p>}
+      {state?.success && (
+        <p className="text-sm text-green-600">Muito obrigado!</p>
+      )}
 
       <Textarea
         placeholder="Abra seu coração ❤️"
         id="message"
         className="h-32"
         name="message"
+        ref={inputRef}
       />
       {state?.error?.message && (
         <p className="text-sm text-red-500">{state.error.message}</p>
