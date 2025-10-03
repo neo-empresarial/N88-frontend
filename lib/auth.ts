addabe6cbde9e3d277fb5168d2edfb793cd3ebcd
JTR <orchulhak.ufsc@gmail.com>
2025-10-03 18:28:15 -0300

    2

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,5 @@
-﻿// lib/auth.ts
-"use server";
-
+﻿"use server";
+import { SignUpFormSchema } from "./type";
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
 import { FormState, SignInFormSchema } from "./type";
@@ -40,4 +39,59 @@ export async function signIn(state: FormState, formData: FormData): Promise<Form
   }
 
   return { message: res.status === 401 ? "Email ou senha incorretos" : "Erro ao autenticar." };
+}
+
+
+async function register(data: data) {
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}auth/register`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+      credentials: "include",
+    });
+    const responseData = await response.json();
+    if (!response.ok) {
+      throw new Error(responseData.message || "Failed to register");
+    }
+
+    return responseData;
+  } catch (error) {
+      console.error("Falha na chamada da API de registro:", error);
+    return {
+      statusCode: 500,
+      message: "Erro de rede. Verifique sua conexão.",
+    };
+  }
+}
+
+export async function signUp(
+  state: FormState,
+  formData: FormData
+): Promise<FormState> {
+  const validationFields = SignUpFormSchema.safeParse({
+    name: formData.get("name"),
+    course: formData.get("course"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
+
+  if (!validationFields.success) {
+    return {
+      error: validationFields.error.flatten().fieldErrors,
+    };
+  }
+
+  const response = await register(validationFields.data);
+
+  if (response.statusCode === 201) {
+    return redirect("signin");
+  }
+
+  return {
+    message:
+      response.statusCode === 409
+        ? "Esse email já está em uso. Tente outro."
+        : response.message || "Ocorreu um erro no servidor.",
+  };
 }
