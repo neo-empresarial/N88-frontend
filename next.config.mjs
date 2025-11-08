88244282d623b76922f99c8238a892c01720967a
Gustavo Torres <gustavotorres@Gustavos-MacBook-Pro.local>
2025-11-08 16:28:17 -0300

    add Docker structure for deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone", // For Docker usage
   env: {
     NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
   },
